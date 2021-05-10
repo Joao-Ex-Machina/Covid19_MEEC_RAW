429cd063ac4aecd5149f9d53dce4cd5e4c0d2453
João Rodrigues <80014156+Joao-Ex-Machina@users.noreply.github.com>
2021-05-10 16:00:56 +0100

    Add files via upload

---
+++ pre-alpha-scripts/modules/covid19_main.c
@@ -0,0 +1,8 @@
+/*-----------------------------------------------------------------------------------------------------+
+| covid19_main.c         |Main module for The Programming Curricular Unit final Project- "Covid19"     |
+|                        |Directives can be found here:                                                |
+|                        |                                                                             |
++------------------------------------------------------------------------------------------------------+
+| Authors: Joao Barreiros C. Rodrigues (Joao-Ex-Machina) nº99968, Henrique "Delfas" Delfino            |
+| Date: 09 May 2021                                                                                    |
++-----------------------------------------------------------------------------------------------------*/
