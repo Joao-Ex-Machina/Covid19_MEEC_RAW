66c433d4daca1004f768e3ada827c37387e9e285
João Rodrigues <80014156+Joao-Ex-Machina@users.noreply.github.com>
2021-05-20 01:30:21 +0100

    Add files via upload

--- pre-alpha-scripts/modules_III/sortingmodule.c
+++ pre-alpha-scripts/modules_III/sortingmodule.c
@@ -6,6 +6,14 @@
 | Authors: Joao Barreiros C. Rodrigues (Joao-Ex-Machina) nº99968, Henrique "Delfas" Delfino            |
 | Date: 19 May 2021                                                                                    |
 +-----------------------------------------------------------------------------------------------------*/
+
+/*Function Name: switcher
+  Input: Two pointers to a struct of type country_list
+  Output: Modified pointer to struct "right" of type country_list
+  Date Created: 19 May 2021
+  Last Revised: 20 May 2021
+  Definition: Basic function to switch the order of two adjacent nodes
+*/
 country_list *switcher(country_list *left,country_list *right ){
     left->next=right->next;
     right->next=left;
